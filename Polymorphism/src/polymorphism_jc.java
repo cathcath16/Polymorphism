7877f9f5e964b19b7460bea43e493b2fbe56358d
cathcath16 <catherenelabinisia@gmail.com>
2023-11-25 20:31:18 +0800

    added and save my jc class

--- Polymorphism/src/polymorphism_jc.java
+++ Polymorphism/src/polymorphism_jc.java
@@ -1,13 +1,13 @@
 package polymorphism;
 
-public class Polymorphism_Zyrel extends Polymorphism_Student{   //child class
-    Polymorphism_Zyrel(String name){
+public class Polymorphism_jc extends Polymorphism_student{   //child class
+    Polymorphism_jc (String name){
         super(name);
     }
-    void coding(){
-        System.out.println(name + " kay galabad ang ulo sa sigeg code");
+    void kapoy(){
+        System.out.println(name + " Gikapoy nas iyang laypppp!");
     }    
-    public void studentCute(){
-        System.out.println("Nagpa CUTE si " + name);
+    public void cutekaayo(){
+        System.out.println("Sige rag pangapi si  " + name);
     }
 }
